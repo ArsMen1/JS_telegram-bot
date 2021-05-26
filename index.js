8c6f1f54b8c8d7604e5a95430fb6a5e6781d9c10
ArsMen1 <71522524+ArsMen1@users.noreply.github.com>
2021-05-26 23:53:39 +0300

    Начал писать игру

--- index.js
+++ index.js
@@ -4,23 +4,43 @@ const token = "1826953714:AAFSy6SkCgsc8LV2DuLxUXA7wnbzpsh5cxY";
 
 const bot = new TelegramApi(token, { polling: true });
 
-bot.on("message", (msg) => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
-  if (text == "/start") {
-    bot.sendMessage(
-      chatId,
+const start = () => {
+  bot.setMyCommands([
+    { command: "/start", description: "Начальное приветствие" },
+    { command: "/info", description: "Информация о пользователе" },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    if (text == "/start") {
+      await bot.sendSticker(
+        chatId,
+        "https://tlgrm.ru/_/stickers/5fb/e86/5fbe8646-6371-463c-ba7d-bbc08ab0b860/3.webp"
+      );
+      return bot.sendMessage(
+        chatId,
+        `
+      Добро пожаловать в телеграмм бота proffi01
+      Welcome to telegram bot proffi01
       `
-    Добро пожаловать в телеграмм бота profi01
-    Welcome to telegram bot profi01
-    `
-    );
-  }
-  if (text == "/start") {
-    bot.sendMessage(
+      );
+    }
+    if (text == "/info") {
+      return bot.sendMessage(
+        chatId,
+        `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
+      );
+    }
+    if (text == "/game") {
+      await bot.sendMessage(chatId, `Угадай какое число от 0 до 9 я загадал)`);
+    }
+    return bot.sendMessage(
       chatId,
-      `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
+      `Я тебя не понимаю попробуй ввести сущесвующую команду`
     );
-  }
-  bot.sendMessage(chatId, `Ты написал мне ${text}`);
-});
+    //bot.sendMessage(chatId, `Ты написал мне ${text}`);  Repeat
+  });
+};
+
+start();
